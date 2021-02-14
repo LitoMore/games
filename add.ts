87491c90041149646bdc7dac584e3a50eb17995e
LitoMore <litomore@gmail.com>
2021-02-14 21:57:08 +0800

    Add auto-add script

---
+++ add.ts
@@ -0,0 +1,36 @@
+import games from "./games.ts";
+
+const [command] = Deno.args;
+if (!command) Deno.exit(1);
+
+const [anchor, name, website] = command.split("\n");
+if (!(anchor && name && website)) Deno.exit(1);
+
+games.devices.forEach((device) => {
+  if (device.anchor.includes(anchor)) {
+    device.gameList.push({
+      name,
+      website,
+    });
+    device.gameList.sort((a, b) => {
+      const aName = a.name.toLowerCase();
+      const bName = b.name.toLowerCase();
+      if (aName < bName) return -1;
+      if (aName > bName) return 1;
+      return 0;
+    });
+  }
+});
+
+const tsFile = `export default ${JSON.stringify(games, null, 2)}`;
+const writePermission = { name: "write", path: "./" } as const;
+const runPermission = { name: "run", path: "./" } as const;
+await Deno.permissions.request(writePermission);
+await Deno.permissions.request(runPermission);
+await Deno.writeTextFile("./games.ts", tsFile);
+const cmd = Deno.run({
+  cmd: ["deno", "fmt", "games.ts"],
+  stdout: "piped",
+});
+await cmd.output();
+cmd.close();
